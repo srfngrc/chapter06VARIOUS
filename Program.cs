885ed6b9b39f958e8c6640bd17aca8cef2b3c61e
sg <srfngrc@outlook.com>
2019-07-08 19:21:58 -0400

    ch06ex03 added 8july 7h21pm

--- Program.cs
+++ Program.cs
@@ -21,26 +21,47 @@ namespace Ch06Ex01_02_etc
     //}
 
     //Ch06Ex02
+    //class Program
+    //{
+
+    //    static int MaxValue(int[] intArray)
+    //    {
+    //        int maxVal = intArray[0];
+    //        for (int i = 1; i < intArray.Length; i++)
+    //        {
+    //            if (intArray[i] > maxVal)
+    //                maxVal = intArray[i];
+    //        }
+    //        return maxVal;
+    //    }
+    //    static void Main(string[] args)
+    //    {
+    //        int[] myArray = { 1, 8, 3, 6, 2, 5, 9, 3, 0, 2 };
+    //        int Srfn = myArray.Max();
+    //        int maxVal = MaxValue(myArray);
+    //        Console.WriteLine("The maximum value in myArray is {0} and the other {1}", maxVal, Srfn);
+    //        Console.ReadKey();
+    //    }
+    //}
+
+    //Ch06Ex03
     class Program
     {
-        
-        static int MaxValue(int[] intArray)
+        static void Write()
         {
-            int maxVal = intArray[0];
-            for (int i = 1; i < intArray.Length; i++)
-            {
-                if (intArray[i] > maxVal)
-                    maxVal = intArray[i];
-            }
-            return maxVal;
+            string myString = "String defined in Write()";
+            Console.WriteLine("Now in Write()");
+            Console.WriteLine("myString = {0}", myString);
         }
         static void Main(string[] args)
         {
-            int[] myArray = { 1, 8, 3, 6, 2, 5, 9, 3, 0, 2 };
-            int Srfn = myArray.Max();
-            int maxVal = MaxValue(myArray);
-            Console.WriteLine("The maximum value in myArray is {0} and the other {1}", maxVal, Srfn);
+            string myString = "String defined in Main()";
+            Write();
+            Console.WriteLine("\nNow in Main()");
+            Console.WriteLine("myString = {0}", myString);
             Console.ReadKey();
         }
     }
 }
+
+
